4406a84c900d68d2e2873bdc2b5952ec9fb9dbf5
mkseo <rhfu89@gmail.com>
2020-06-24 11:35:29 +0900

    unttest

--- functional_test.py
+++ functional_test.py
@@ -2,6 +2,5 @@ from selenium import webdriver
 
 browser = webdriver.Firefox(executable_path='C:/work/untitled/geckodriver.exe')
 browser.get('http://localhost:8000')
-#browser.get('http://www.naver.com')
 
-assert 'Django' in browser.title
+assert 'TO-Do' in browser.title, "Browser title was " + browser.title
